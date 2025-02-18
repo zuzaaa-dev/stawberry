0d9caa9d559af9a7789ed7122dea60c7a70e9b4b
Avalance <avalance228@gmail.com>
2025-02-18 22:27:58 +0700

    update

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -3,8 +3,6 @@ package repository
 import (
 	"log"
 
-	"github.com/zuzaaa-dev/stawberry/internal/repository/model"
-
 	"github.com/zuzaaa-dev/stawberry/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,18 +12,6 @@ func InitDB(cfg *config.Config) *gorm.DB {
 	db, err := gorm.Open(postgres.Open(cfg.GetDBConnString()), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
-	}
-
-	// Auto migrate schemas
-	err = db.AutoMigrate(
-		&model.User{},
-		&model.Store{},
-		&model.Product{},
-		&model.Offer{},
-		&model.Notification{},
-	)
-	if err != nil {
-		log.Fatal("Failed to migrate database:", err)
 	}
 
 	return db
